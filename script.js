7d2958447ca45fef4e84a6e7dc58010378b84941
Soundar Rajan L <sdjthedeveloper@gmail.com>
2021-01-14 13:39:13 +0530

    stop media

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.getElementById('openCamera').addEventListener('click', e => openCamera(e));
+document.getElementById('stopCamera').addEventListener('click', e => stopCamera(e));
+let stream;
+
+function stopCamera() {
+	if (!stream) {
+		console.warn('no stream to stop!');
+		return;
+	}
+	stream.getTracks().forEach(function(track) {
+		track.stop();
+	});  
+}
 
 function openCamera() {
 	const localVideoElem = document.getElementById('localVideo');
 	navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
 		localVideoElem.srcObject = stream;
+		stream = stream;
 	}).catch(e => {
 		console.error(e);
 		const errorElem = document.getElementById('error');
